bbf9150ef1eef6233df189879899839e079e65b1
Imron02 <imron02@gmail.com>
2020-12-30 15:25:57 +0700

    Refactor(app_test): change from act to waitfor

--- __tests__/app_test.tsx
+++ __tests__/app_test.tsx
@@ -2,14 +2,12 @@ import 'react-native';
 import React from 'react';
 
 import App from '../App';
-import { act, render } from '@testing-library/react-native';
+import { render, waitFor } from '@testing-library/react-native';
 import '@testing-library/jest-native/extend-expect';
 
 it('renders correctly', async () => {
   const { getByRole } = render(<App />);
-  const header = getByRole('header');
+  const header = await waitFor(() => getByRole('header'));
 
-  await act(async () => {
-    expect(header).toHaveTextContent('Login');
-  });
+  expect(header).toHaveTextContent('Login');
 });
