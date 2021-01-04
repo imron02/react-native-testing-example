69243e2f04f0d50bc2996eb395bac925b4615f03
Imron02 <imron02@gmail.com>
2021-01-04 14:46:21 +0700

    Refactor(app_test): integration redux provider

--- __tests__/app_test.tsx
+++ __tests__/app_test.tsx
@@ -1,12 +1,18 @@
 import 'react-native';
 import React from 'react';
-
-import App from '../App';
 import { render, waitFor } from '@testing-library/react-native';
+import { Provider } from 'react-redux';
 import '@testing-library/jest-native/extend-expect';
 
+import App from '../App';
+import store from '../src/redux/store';
+
 it('renders correctly', async () => {
-  const { getByRole } = render(<App />);
+  const { getByRole } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   const header = await waitFor(() => getByRole('header'));
 
   expect(header).toHaveTextContent('Login');
