ee6bcb89dc10685d05a7ce95cf99468b4200b185
Imron02 <imron02@gmail.com>
2021-01-04 14:46:34 +0700

    Refactor(login_action_test): add test for login_action

---
+++ __tests__/login_action_test.tsx
@@ -0,0 +1,20 @@
+import { appInit, signIn, signOut } from '../src/redux/login_action';
+import { APP_INIT, SIGN_IN, SIGN_OUT } from '../src/utils/constant_util';
+
+describe('login action', () => {
+  it('returns action with type APP_INIT', () => {
+    const action = appInit();
+    expect(action).toEqual({ type: APP_INIT });
+  });
+
+  it('returns action with type SIGN_IN', () => {
+    const token = 'dummy-token';
+    const action = signIn(token);
+    expect(action).toEqual({ type: SIGN_IN, payload: token });
+  });
+
+  it('returns action with type SIGN_OUT', () => {
+    const action = signOut();
+    expect(action).toEqual({ type: SIGN_OUT });
+  });
+});
